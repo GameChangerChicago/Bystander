eff2a4f49106a651442fbc166a09c5c0d08f3652
mwarrington <mason.arrington@gmail.com>
2014-10-31 11:40:21 -0500

    Party Scenario Framework

---
+++ Bystander/Assets/Scenarios/PartyScenario/Scripts/PartyGameController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PartyGameController : MonoBehaviour
+{
+    private PartyGameManager _myGameManager;
+
+    void Start()
+    {
+        _myGameManager = FindObjectOfType<PartyGameManager>();
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Bystander/Assets/Scenarios/PartyScenario/Scripts/PartyGameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PartyGameManager : MonoBehaviour
+{
+    private PartyCameraManager _myCameraManager;
+
+    void Start()
+    {
+        _myCameraManager = FindObjectOfType<PartyCameraManager>();
+    }
+
+    void Update()
+    {
+
+    }
+}

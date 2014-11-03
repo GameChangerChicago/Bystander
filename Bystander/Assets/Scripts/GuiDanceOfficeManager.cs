50e3b733bc9a864f9c89324ba9b7fad3516a10ac
ci3-122-mac2-taylorjames9 <taylorjames9@gmail.com>
2014-11-03 14:52:59 -0600

    Guidance Scene Manager Script now working

---
+++ Bystander/Assets/Scripts/GuiDanceOfficeManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiDanceOfficeManager : MonoBehaviour {
+
+
+	//This script registers mouseClicks any where on the screen to advance the dialogue
+	//Implemented.
+
+	//This script contains a state machine that dictates the states of the scene.
+	//Everytime the int guidanceSceneState variable changes...it's property variable triggers
+	//the getter and setter methods in the STEVE and FAITH scripts to update their dialogue. 
+
+
+	private SteveScript steveScript;
+	private FaithScript faithScript;
+
+	private int guidanceSceneState;
+	public int GuidanceSceneState {
+
+		get
+		{
+			//Some other code
+			return guidanceSceneState;
+		}
+		set
+		{
+			//Some other code
+			guidanceSceneState = value;
+		}
+
+	}
+
+	void Start () {
+		faithScript = GetComponent<FaithScript>();
+		steveScript = GetComponent<SteveScript>();
+	}
+
+
+	void OnMouseDown(){
+		GuidanceSceneState++;
+		Debug.Log ("guidanceScene State is : "+GuidanceSceneState);
+	}
+
+
+
+
+
+}
--- Bystander/Assets/Scripts/SteveScript.cs
+++ Bystander/Assets/Scripts/SteveScript.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TestScript : MonoBehaviour {
+public class SteveScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {

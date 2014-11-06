ef745b9ff146781ce4bf0715a1561ca32434fb52
ci3-122-mac2-taylorjames9 <taylorjames9@gmail.com>
2014-11-06 11:21:30 -0600

    NGUI installed. GuidanceCounselorScene almost redone.

---
+++ Bystander/Assets/Scenarios/SexualHarassment_Redo/Scripts/FaithScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+//using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FaithScript : MonoBehaviour {
+
+
+
+
+
+	public GUIStyle myGUIStyle;
+
+	public GUIText mySpeechText; 
+
+
+	public GameObject myManager;
+	private GuiDanceOfficeManager myManagerScript;
+	private List<string> faithLines = new List<string>();
+	Dictionary<int, string> badguys = new Dictionary<int, string>();
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		myManagerScript = myManager.GetComponent<GuiDanceOfficeManager>();
+
+		faithLines.Add ("I don't know how to talk about it ...fklajdslkfj"); 
+		faithLines.Add ("I don't know how to talk about itaksdjflkadfjlajdflkjaldfjlakjdfljl"); 
+		faithLines.Add ("adsfhkadfj   It's just one of those things...fajslkdfjlakjdf"); 
+		faithLines.Add ("adsfhkadfj   It's just one of those things...fajslkdfjlakjdf"); 
+		faithLines.Add ("adsfhkadfj   It's just one of those things...fajslkdfjlakjdf"); 
+		faithLines.Add ("I'm distracted, can't concentrateajskldfjladjflkajdflk"); 
+		faithLines.Add ("I don't know if I should. dklajlksdjflkajdlksfjlkadf"); 
+		faithLines.Add ("I don't know if I should. dklajlksdjflkajdlksfjlkadf"); 
+		faithLines.Add ("It's subtle. But it's there. It's everywhere. See for yourself."); 
+
+	}
+	
+	public void displayCorrectDialogue(int speechNum){
+		if (speechNum < faithLines.Count) {
+			Debug.Log ("We are inside of displayCorrectDialogue for faith");
+			mySpeechText.text = faithLines [speechNum];
+		}
+
+	}
+}

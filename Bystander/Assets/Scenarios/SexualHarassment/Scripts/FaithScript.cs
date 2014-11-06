bcf605dbf1054f4e73624ea386cd05c6f35bddbe
ci3-122-mac2-taylorjames9 <taylorjames9@gmail.com>
2014-11-06 13:20:21 -0600

    Bystander Guidance Counselor Frame. Working W/NGUI and DICTIONARIES for dialogue. Cleaned up some of the scripts for legibility.

--- Bystander/Assets/Scenarios/SexualHarassment/Scripts/FaithScript.cs
+++ Bystander/Assets/Scenarios/SexualHarassment/Scripts/FaithScript.cs
@@ -6,34 +6,18 @@ using System.Collections.Generic;
 public class FaithScript : MonoBehaviour {
 
 
-
-
-
-	//public GUIStyle myGUIStyle;
-
-	//public GUIText mySpeechText; 
-
 	public UILabel mySpeechText; 
 
 	private int faithLinesCounter;
-
-
-	public GameObject myManager;
-	private GuiDanceOfficeManager myManagerScript;
-	//private List<string> faithLines = new List<string>();
 	Dictionary<int, string> faithLines = new Dictionary<int, string>();
 
-
-	// Use this for initialization
 	void Start () {
-
-		myManagerScript = myManager.GetComponent<GuiDanceOfficeManager>();
-
+	
 		faithLines.Add (2, "I don't know how to talk about it ...fklajdslkfj"); 
 		faithLines.Add (4, "adsfhkadfj   It's just one of those things...fajslkdfjlakjdf"); 
 		faithLines.Add (6, "I'm distracted, can't concentrateajskldfjladjflkajdflk"); 
 		faithLines.Add (8, "I don't know if I should. dklajlksdjflkajdlksfjlkadf"); 
-		faithLines.Add (10,"I don't know if I should. dklajlksdjflkajdlksfjlkadf"); 
+		faithLines.Add (10,"It's not so simple."); 
 		faithLines.Add (12, "It's subtle. But it's there. It's everywhere. See for yourself."); 
 
 	}
--- Bystander/Assets/Scenarios/SexualHarassment/Scripts/SteveScript.cs
+++ Bystander/Assets/Scenarios/SexualHarassment/Scripts/SteveScript.cs
@@ -8,32 +8,24 @@ public class SteveScript : MonoBehaviour {
 	public UILabel mySpeechText; 
 
 	private int steveLinesCounter;
-
-	public GameObject myManager;
-	private GuiDanceOfficeManager myManagerScript;
-
 	Dictionary<int, string> steveLines = new Dictionary<int, string>();
-
-	//private List<string> steveLines = new List<string>();
 
 	// Use this for initialization
 	void Start () {
-
-		myManagerScript = myManager.GetComponent<GuiDanceOfficeManager>();
-
+	
 		steveLines.Add (1, "Wha ");
 		steveLines.Add (3,"What's the matter? ajsdlfkjalkdjflkjlaksdfjlajdlfjlajljsdlfjlkajldjfljalkdsjflkjakldfjkljaldjflkjalkjsdlkfjlkjalkjsldkfjlkjalksjdlfkjlkjlka"); 
 		steveLines.Add (5, "(Clears throat)*"); 
 		steveLines.Add (7, "Well, here we are. At the counselor's office"); 
 		steveLines.Add (9,"Step up. It's time to shed some light on this."); 
-		steveLines.Add (11, "I can only lead you so far."); 
-		steveLines.Add (13, "Do you see it a lot? In the halls and stuff?"); 
+		steveLines.Add (10,"I can only lead you so far."); 
+		steveLines.Add (11, "Do you see it a lot? In the halls and stuff?"); 
 
 	}
 
 	public void displayCorrectDialogue(){
-		steveLinesCounter++;
 
+		steveLinesCounter++;
 		string temp = null;
 
 		if (steveLines.TryGetValue(steveLinesCounter, out temp)) {

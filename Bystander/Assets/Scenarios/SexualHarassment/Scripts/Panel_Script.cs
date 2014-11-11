1241246d28762229964d0b3d7765121a28b50e0e
ci3-122-mac2-taylorjames9 <taylorjames9@gmail.com>
2014-11-11 13:56:32 -0600

    Working. Simplified panel scripts

--- Bystander/Assets/Scenarios/SexualHarassment/Scripts/Panel_Script.cs
+++ Bystander/Assets/Scenarios/SexualHarassment/Scripts/Panel_Script.cs
@@ -3,25 +3,34 @@ using System.Collections;
 
 public class Panel_Script : MonoBehaviour {
 
-
+	public Transform mySituationToLoad;
 	public GameObject target;
 	public static readonly float offset = 20.0f;
-	public float speed = 10.0f;
+	private float speed;
 	private Vector3 newPosition;
 
 	void Start(){
 		int rand1 = Random.Range(0,2);
-		float rand2 = Random.Range(10f,200f);
+		float rand2 = Random.Range(10f,500f);
+
+		speed = 3.0f;
 
 		if (rand1 == 0) {
 			transform.position = new Vector3 (transform.position.x + rand2, transform.position.y, 0);
 		}
 		else
 			transform.position = new Vector3 (transform.position.x, transform.position.y+ rand2, 0);
-
 	}
 		
 	void Update(){
-		transform.position = Vector3.Lerp(transform.position, target.transform.position, 3*Time.deltaTime);
+		transform.position = Vector3.Lerp(transform.position, target.transform.position, speed*Time.deltaTime);
+	}
+
+	void OnMouseDown(){
+		loadMySituation ();
+	}
+
+	public void loadMySituation (){
+		//load the scene/situation prefab specific to this script
 	}
 }

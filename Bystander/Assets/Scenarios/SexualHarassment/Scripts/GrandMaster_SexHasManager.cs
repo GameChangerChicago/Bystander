cd75f9230e7b2ff2a51b2c7230bbcdb05b32f76a
ci3-122-mac2-taylorjames9 <taylorjames9@gmail.com>
2014-11-11 13:40:36 -0600

    WORKING. Panels now slide into place

--- Bystander/Assets/Scenarios/SexualHarassment/Scripts/GrandMaster_SexHasManager.cs
+++ Bystander/Assets/Scenarios/SexualHarassment/Scripts/GrandMaster_SexHasManager.cs
@@ -6,5 +6,9 @@ public class GrandMaster_SexHasManager : MonoBehaviour {
 	/*The primary job of this script is to keep track of how many sections of the 
 	 * app have been completed altogether.*/
 
+	//And to generally make sure the user is in the right part of the app at the right time
+
+
+
 
 }
---
+++ Bystander/Assets/Scenarios/SexualHarassment/Scripts/Panel_Script.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Panel_Script : MonoBehaviour {
+
+
+	public GameObject target;
+	public static readonly float offset = 20.0f;
+	public float speed = 10.0f;
+	private Vector3 newPosition;
+
+	void Start(){
+		int rand1 = Random.Range(0,2);
+		float rand2 = Random.Range(10f,200f);
+
+		if (rand1 == 0) {
+			transform.position = new Vector3 (transform.position.x + rand2, transform.position.y, 0);
+		}
+		else
+			transform.position = new Vector3 (transform.position.x, transform.position.y+ rand2, 0);
+
+	}
+		
+	void Update(){
+		transform.position = Vector3.Lerp(transform.position, target.transform.position, 3*Time.deltaTime);
+	}
+}

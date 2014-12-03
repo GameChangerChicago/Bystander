273968a5029a868f3ebc73bc48e81d36dfd9c968
kaira2006 <kayura60@gmail.com>
2014-12-03 10:06:31 -0600

    Webpage Assets and Framework for Male (LinkedList)
    Added webpage assets and basic click functionality for Male Scenario.
    Can click on “participate” link for new page.

---
+++ Bystander/Assets/Scenarios/MaleScenario/Scripts/Node.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node : MonoBehaviour {
+	
+		private Node next;
+		private Object data;
+		
+		public Node(this Object data){
+			next = null;
+			data = data;
+		}
+		
+		public Node(this Object data, Node nextValue){
+			next = nextValue;
+			data = data;
+			
+		}
+		
+		
+		
+		public Object getNode() {
+			return data; 
+		}
+		
+		public void setNode(Object data){
+			data = data;
+		}
+		
+		public Node getNext(){
+			return next;
+		}
+		
+		public void setNext(Node nextValue){
+			next = nextValue;
+		}
+	}
+

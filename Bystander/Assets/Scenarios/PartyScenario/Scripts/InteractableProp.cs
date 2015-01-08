4fcbb4204d2d990bd4032628eed9c219cea6de44
mwarrington <mason.arrington@gmail.com>
2015-01-08 15:31:50 -0600

    Cleaned and commented InteractableProp.cs

--- Bystander/Assets/Scenarios/PartyScenario/Scripts/InteractableProp.cs
+++ Bystander/Assets/Scenarios/PartyScenario/Scripts/InteractableProp.cs
@@ -21,34 +21,37 @@ public class InteractableProp : MonoBehaviour
     private int _myClickCount = 0;
     private string _dialog;
     private PartyGameManager _myGameManager;
-    private Vector3 _myInitialSize;
 
     void Start()
     {
         _myGameManager = FindObjectOfType<PartyGameManager>();
-        _myInitialSize = this.GetComponent<BoxCollider>().size;
     }
 
     void OnMouseDown()
     {
         if (MaxClicks != _myClickCount)
         {
+            //Interactable Props with multiple steps need to call different animations each time
             if (HasMultipleSteps)
             {
                 animation.clip = animation.GetClip(this.name + _myClickCount);
                 Invoke("ChangeAnimation", CameraMoveTime);
-            }
+            } //Otherwise, don't worry about it
             else if (AnimationChanges)
                 Invoke("ChangeAnimation", CameraMoveTime);
 
+            //This calls PlayerClicked sending all of the impertinent data
             _myGameManager.PlayerClicked(ImportantProp, HasDialog, CameraMoveTime, MyPanelPos.position, CameraSize, ViewTime);
 
+            //If the interactable prop has dialog than this calls the first DialogHandler
             if(HasDialog)
             {
                 ConvoHandler currentCloseUpConvo = GameObject.Find("CloseUpPanel_" + this.name).GetComponent<ConvoHandler>();
                 StartCoroutine(currentCloseUpConvo.DialogHandler(CameraMoveTime));
                 _myClickCount++;
             }
+
+            //If the interactable prop has dialog than this calls PlayCloseUpAnimation
             if (HasCloseupAnimation)
             {
                 StartCoroutine(_myGameManager.PlayCloseUpAnimation(CloseUpAnimation, _myClickCount, CameraMoveTime));

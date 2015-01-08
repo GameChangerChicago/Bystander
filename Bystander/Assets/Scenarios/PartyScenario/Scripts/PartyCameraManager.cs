d51ef30e181b3c1bf4bc9197edb4da813f843973
mwarrington <mason.arrington@gmail.com>
2015-01-08 16:02:06 -0600

    Cleaned and commented PartyCameraManager.cs

--- Bystander/Assets/Scenarios/PartyScenario/Scripts/PartyCameraManager.cs
+++ Bystander/Assets/Scenarios/PartyScenario/Scripts/PartyCameraManager.cs
@@ -11,37 +11,41 @@ public class PartyCameraManager : MonoBehaviour
                   _camSizeDiff;
     private bool _movingCamera;
 
-    void Start()
-    {
-        
-    }
-
     void Update()
     {
+        //MoveCameraTo() needs to happen in an update to function so its regulated by the bool _movingCamera
         if (_movingCamera)
         {
             MoveCameraTo();
         }
     }
 
+    //This method handles moving the camera from place to place and also handles camera size
     private void MoveCameraTo()
     {
+        //Using the iTween method MoveTo we set an object to move, a location, and a speed; iTween handles the rest
         iTween.MoveTo(this.gameObject, new Vector3(_pos.x, _pos.y, this.transform.position.z), _camTravelTime);
 
+        //If the current camera size is greater than _camSize then we know we need to decrease the size of the camera
         if (this.camera.orthographicSize > _camSize)
         {
             this.camera.orthographicSize -= _camSizeDiff / (_camTravelTime / Time.deltaTime);
+
+            //Eventually the camera size will be slightly smaller than the desired size; once this happens we simply set the size to _camSize
             if (this.camera.orthographicSize < _camSize)
                 this.camera.orthographicSize = _camSize;
         }
-        else if (this.camera.orthographicSize < _camSize)
+        else if (this.camera.orthographicSize < _camSize) //If the current camera size is less than _camSize then we know we need to increase the size of the camera
         {
             this.camera.orthographicSize += _camSizeDiff / (_camTravelTime / Time.deltaTime);
+
+            //Eventually the camera size will be slightly larger than the desired size; once this happens we simply set the size to _camSize
             if (this.camera.orthographicSize > _camSize)
                 this.camera.orthographicSize = _camSize;
         }
     }
 
+    //This method initializes the moving process; setting impertinant fields and also invokes StopMoving
     public void SetCameraToMove(Vector3 pos, float camTravelTime, float camSize)
     {
         _pos = pos;
@@ -52,6 +56,7 @@ public class PartyCameraManager : MonoBehaviour
         Invoke("StopMoving", camTravelTime);
     }
 
+    //This method moves the camera back to it's orrigin pos and size; also it invokes StopMoving
     public IEnumerator ReturnCamera(Vector3 pos, float viewTime)
     {
         yield return new WaitForSeconds(viewTime);
@@ -67,6 +72,7 @@ public class PartyCameraManager : MonoBehaviour
         Invoke("StopMoving", _camTravelTime);
     }
 
+    //StopMoving stops the movement of the camera by setting _movingCamera, a bool used to determin whether the camera is moving, to false which will cause the camera, which up to this point was moving, to stop the afformentioned moving. This is useful for when we want the camera to no longer move.
     private void StopMoving()
     {
         _movingCamera = false;

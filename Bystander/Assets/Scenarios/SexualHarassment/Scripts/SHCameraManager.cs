2099c382cbae84e1e2a72c7283e21f286e0cc15a
mwarrington <mason.arrington@gmail.com>
2015-01-09 14:15:31 -0600

    Cleaned, commented, and sreamlinned SHCameraManager.cs

--- Bystander/Assets/Scenarios/SexualHarassment/Scripts/SHCameraManager.cs
+++ Bystander/Assets/Scenarios/SexualHarassment/Scripts/SHCameraManager.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class SHCameraManager : MonoBehaviour
 {
     public Vector2 cameraPos;
+
+    //This property handles which camera is being shown and where it should be when it's not the main camera
     public bool IsCurrentCamera
     {
         get
@@ -12,17 +14,20 @@ public class SHCameraManager : MonoBehaviour
         }
         set
         {
+            //If the property is being set to true...
             if (value)
             {
+                //The camera's rectangle is position that it encompaces the scene
                 this.camera.rect = new Rect(0, 0, 1, 1);
 
+                //It then turns off every other camera
                 for (int i = 0; i < FindObjectsOfType<SHCameraManager>().Length; i++)
                 {
                     if (FindObjectsOfType<SHCameraManager>()[i] != this)
                         this.camera.enabled = false;
                 }
             }
-            else
+            else //When set false the camera is moved and shrunken to it's hold position
             {
                 this.camera.rect = new Rect(cameraPos.x, cameraPos.y, 0.2f, 0.2f);
             }
@@ -36,14 +41,11 @@ public class SHCameraManager : MonoBehaviour
 
     void Start()
     {
+        //Standard initialization junk
         _myBoxCollider = this.GetComponent<BoxCollider2D>();
     }
 
-    void Update()
-    {
-
-    }
-
+    //This method sets IsCurrentCamera to true for this SHCameraManager and sets IsCurrentCamera to false for all others
     public void FocusOnCamera()
     {
         for (int i = 0; i < FindObjectsOfType<SHCameraManager>().Length; i++)
@@ -55,9 +57,10 @@ public class SHCameraManager : MonoBehaviour
         }
     }
 
+    //This method sets IsCurrentCamera to false and turns on the grayscale effect 
     public void ReturnToHub()
     {
         IsCurrentCamera = false;
-        this.GetComponentInChildren<SpriteRenderer>().enabled = true;
+        this.GetComponent<GrayscaleEffect>().enabled = true;
     }
 }

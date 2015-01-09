6fe1027ac5b6e1959bfcbf90d5f59e3dcb6326ac
mwarrington <mason.arrington@gmail.com>
2015-01-09 12:29:41 -0600

    Cleaned up and commented PointOfInterest.cs

--- Bystander/Assets/Scenarios/SexualHarassment/Scripts/PointOfInterest.cs
+++ Bystander/Assets/Scenarios/SexualHarassment/Scripts/PointOfInterest.cs
@@ -17,18 +17,23 @@ public class PointOfInterest : MonoBehaviour
 
     void Start()
     {
+        //Standard instantiation junk. UP, UP, DOWN, DOWN, LEFT, RIGHT, LEFT, RIGHT. WHATCHU GONNA DO NOW? A, B, START! -Konami
         _myGameManager = FindObjectOfType<SHGameManager>();
         _myVirgil = FindObjectOfType<SHVigilHandler>();
+
+        //The .txt used for virgil's responses and the prefabs for the mini comic are named after the PointOfInterest in question
         _virgilString = Resources.Load("SHText/VirgilDialog_" + this.name).ToString();
         myMiniComic = Resources.Load("Prefabs/MiniComic_" + this.name) as GameObject;
     }
 
+    //If the player clicks and the comic isn't already being shown then the ShowComic is called
     void OnMouseDown()
     {
         if (!ComicShown)
             ShowComic();
     }
 
+    //This method simply instantiates the mini comic in question and starts the Invoke for ShowQuiz
     private void ShowComic()
     {
         _myMiniComic = (GameObject)Instantiate(myMiniComic, InstantiationTransform.position, Quaternion.identity);
@@ -36,17 +41,23 @@ public class PointOfInterest : MonoBehaviour
         ComicShown = true;
     }
 
+    //ShowQuiz handles showing the quiz and setting up it's quiz buttons
     private void ShowQuiz()
     {
+        //These lines destroy the minicomic and moves the quiz into postion
         GameObject quiz = GameObject.Find("Quiz");
         Destroy(_myMiniComic);
         quiz.transform.position = InstantiationTransform.position;
+
+        //Set's the correct dialog string in Virgil
         _myVirgil.DialogString = _virgilString;
 
+        //This for loop sets the impertinant bools for each QuizButton based on whether the point of interest was sexual harassment
         foreach (QuizButton qb in quiz.GetComponentsInChildren<QuizButton>())
         {
             if (IsSexualHarassment)
             {
+                //If the answer is  yes and it is a case of sexual harrasment then the qb will be marked as a Game winner
                 if (qb.name == "Answer Yes")
                 {
                     qb.CorrectAnswer = true;

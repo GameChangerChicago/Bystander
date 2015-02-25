53d02436ef4036892eb901358f651626a8f1819f
kaira2006 <kayura60@gmail.com>
2015-02-24 18:33:18 -0600

    Added Camera Manager + Updated GUIManager
    Finalizing DialoguePopUp + Animation System.  Mainly just transferring
    to laptop…

--- Bystander/Assets/Scenarios/MaleScenario/Scripts/GUIManager.cs
+++ Bystander/Assets/Scenarios/MaleScenario/Scripts/GUIManager.cs
@@ -7,40 +7,53 @@ public class GUIManager : MonoBehaviour
 		private MaleGameManager gameManager;
 		private string inputField;
 		private bool questionAnswered;
+		private int screenWidth, screenHeight;
 
-		void Start ()
+
+	void Start ()
+	{
+		screenWidth = Screen.width / 2;
+		screenHeight = Screen.height / 2;
+		gameManager = FindObjectOfType<MaleGameManager> ();
+		inputField = "Answer";
+		questionAnswered = false;
+	}
+
+
+	void OnGUI ()
+	{
+
+
+		if(!gameManager.isGameState(GameState.Intro) && !gameManager.isGameState(GameState.Outro))
 		{
-				gameManager = FindObjectOfType<MaleGameManager> ();
-				inputField = "Answer";
-				questionAnswered = false;
+			if (Event.current.keyCode == KeyCode.Return && Event.current.type == EventType.KeyDown && !questionAnswered) {
+					gameManager.CheckAnswer (inputField.ToUpper ());
+				    questionAnswered = true;
+					
+			}
+
+        if (Event.current.type == EventType.KeyUp)
+        {
+            questionAnswered = false;
+
+        }
+
+			inputField = GUI.TextField (new Rect (screenWidth - 120, screenHeight - 45 , 125, 30), inputField, 15);
 		}
+	}
 
-		void OnGUI ()
-		{
-
-				if (Event.current.keyCode == KeyCode.Return && Event.current.type == EventType.KeyDown && !questionAnswered) {
-						gameManager.CheckAnswer (inputField.ToUpper ());
-						Debug.Log ("Test");
-                        questionAnswered = true;
-				}
-
-                if (Event.current.type == EventType.KeyUp)
-                {
-                    questionAnswered = false;
-                }
-
-				inputField = GUI.TextField (new Rect (475, 300, 150, 30), inputField, 15);
+	void Update(){
 
 
 
 
-		}
-		
-		public void IntroText ()
-		{
-				Debug.Log ("Here is some intro text");
-				
-		}
+	}
+	
+	private void IntroText ()
+	{
+			Debug.Log ("Here is some intro text");
+			
+	}
 
 
 }
--- Bystander/Assets/Scenarios/MaleScenario/Scripts/MaleGameManager.cs
+++ Bystander/Assets/Scenarios/MaleScenario/Scripts/MaleGameManager.cs
@@ -1,30 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//Enum to track game state - Intro, question one, question two etc.
+//Moved for global accessability
+public enum GameState
+{
+	Intro, 
+	Q1, 
+	Q2, 
+	Q3, 
+	Q4, 
+	Q5, 
+	Q6, 
+	Q7, 
+	Outro
+};
 
 
+public class MaleGameManager : MonoBehaviour
+{
 
-public class MaleGameManager : MonoBehaviour {
+		
+		private string correctAnswer;
+		private bool questionAnswered;
 
-private GUIManager guiManager;
-private string correctAnswer;
+		
+		
+		public static GameState gameState;
 
-	//Enum to track game state - Intro, question one, question two etc.
-	private enum GameState{
-		Intro, 
-		Q1, 
-		Q2, 
-		Q3, 
-		Q4, 
-		Q5, 
-		Q6, 
-		Q7, 
-		Outro
-	};
-
-    //MASON NOTE: This array is redundant. In the switch you can just have correctAnswer = a. I left a commented out example below in the "BELIEVE" case.
-	//Holds correct answers for each question
-	private string[] answerArray	= new string[]{ 
+		//MASON NOTE: This array is redundant. In the switch you can just have correctAnswer = a. I left a commented out example below in the "BELIEVE" case.
+		//Holds correct answers for next question asked
+		private string[] answerArray = new string[]{ 
 		"BELIEVE", 
 		"23%", 
 		"2 MILLION",
@@ -34,93 +40,97 @@ private string correctAnswer;
 		"ALCOHOL",
 		"1-800-656-HOPE"
 	};
-
-
-	private GameState gameState;
-	private bool questionAnswered;
-
-
-	void Start(){
-
-		guiManager = FindObjectOfType<GUIManager>();
-		gameState = GameState.Intro;
-		Invoke ("StartGame", 1f);
-
-
-
-	}
-
-
-	void StartGame(){
-
-		guiManager.IntroText();
-		questionAnswered = false;
-		gameState = GameState.Q1;
-		correctAnswer = "BELIEVE";
 		
 
+		void Start ()
+		{
+			gameState = GameState.Intro;
+			Invoke ("StartGame", 5f);
 		}
 
-	//if the player's answer is correct, we change the game state and set the correct answer
-	//for the new question
-	public void CheckAnswer(string a){
+	
 
-		if (a.Equals (correctAnswer)) {
+		void StartGame ()
+		{
+			questionAnswered = false;
+			gameState = GameState.Q1;
+			correctAnswer = "BELIEVE";
 
-			questionAnswered = true;
+		}
 
-			switch (a) {
+		public bool isGameState(GameState state)
+		{
 
-				case "BELIEVE":
-					Debug.Log ("The Answer is Believe! Moving to next game State");
-                    //correctAnswer = a;
-					correctAnswer = answerArray[1];
-					questionAnswered = false;
-					gameState = GameState.Q2;
-					break;
-				case "23%":
-					Debug.Log ("The Answer is 23%! Moving to next game State");
-					correctAnswer = answerArray[2];
-					questionAnswered = false;
-					gameState = GameState.Q3;
-					break;
-				case "2 MILLION":
-					Debug.Log ("Correct!  You are so smart");
-					correctAnswer = answerArray[3];
-					questionAnswered = false;
+			if(gameState == state)
+				return true;
+			else return false;
+	
+		}
+
+		
+		//if the player's answer is correct, we change the game state and set the correct answer
+		//for the new question
+		public void CheckAnswer (string a)
+		{
+
+				if (a.Equals (correctAnswer)) {
+
+						questionAnswered = true;
+
+						switch (a) {
+
+						case "BELIEVE":
+								Debug.Log ("The Answer is Believe! Moving to next game State");
+								//correctAnswer = a;
+								correctAnswer = answerArray[1];
+								questionAnswered = false;
+								gameState = GameState.Q2;
+								Debug.Log (correctAnswer);
+								break;
+						case "23%":
+								Debug.Log ("The Answer is 23%! Moving to next game State");
+								correctAnswer = answerArray [2];
+								questionAnswered = false;
+								gameState = GameState.Q3;
+								break;
+						case "2 MILLION":
+								Debug.Log ("Correct!  You are so smart");
+								correctAnswer = answerArray [3];
+								questionAnswered = false;
 					//Since this is a two part question, we will not change the game state
-					break;
-				case "STATE":
-					Debug.Log ("Correct! You are really smart");
-					correctAnswer = answerArray[4];
-					gameState = GameState.Q4;
-					break;
-				case "CONSENT":
-					Debug.Log ("Yep yep!");
-					correctAnswer = answerArray[5];
-					gameState = GameState.Q5;
-					break;
-				case "THREATENED":
-					Debug.Log ("Uh-huh!");
-					correctAnswer = answerArray[6];
-					gameState = GameState.Q6;
-					break;
-				case "ALCOHOL":
-					Debug.Log ("Yes~");
-					correctAnswer = answerArray[7];
-					gameState = GameState.Q7;
-					break;
-				case "1-800-656-HOPE":
-					Debug.Log ("Say what!");
-					gameState = GameState.Outro;
-					break;
-				}
+								break;
+						case "STATE":
+								Debug.Log ("Correct! You are really smart");
+								correctAnswer = answerArray [4];
+								gameState = GameState.Q4;
+								break;
+						case "CONSENT":
+								Debug.Log ("Yep yep!");
+								correctAnswer = answerArray [5];
+								gameState = GameState.Q5;
+								break;
+						case "THREATENED":
+								Debug.Log ("Uh-huh!");
+								correctAnswer = answerArray [6];
+								gameState = GameState.Q6;
+								break;
+						case "ALCOHOL":
+								Debug.Log ("Yes~");
+								correctAnswer = answerArray [7];
+								gameState = GameState.Q7;
+								break;
+						case "1-800-656-HOPE":
+								Debug.Log ("Say what!");
+								gameState = GameState.Outro;
+								break;
+						}
 
-			} else
-				Debug.Log ("Wrong Answer. Try Again");
+				} else
+						Debug.Log ("Wrong Answer. Try Again");
 
 		}
 
+		
 
 
 

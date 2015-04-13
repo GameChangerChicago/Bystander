c75e5decbc7e7dc5cdf8f2e79d081e8a50d298d5
mwarrington <mason.arrington@gmail.com>
2015-04-13 16:06:02 -0500

    Classroom almost done

---
+++ Bystander/Assets/Scenarios/SexualHarassment/Scripts/CollisionManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionManager : MonoBehaviour
+{
+
+    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
--- Bystander/Assets/Scenarios/SexualHarassment/Scripts/PointOfInterest.cs
+++ Bystander/Assets/Scenarios/SexualHarassment/Scripts/PointOfInterest.cs
@@ -47,19 +47,6 @@ public class PointOfInterest : MonoBehaviour
         myMiniComic = Resources.Load("Prefabs/MiniComic_" + this.name) as GameObject;
     }
 
-    //If the player clicks and the comic isn't already being shown then the ShowComic is called
-    void OnMouseUp()
-    {
-        if (!ComicShown && !_myGameManager.FocusedOnPOI)
-        {
-            _myMiniComic = (GameObject)Instantiate(myMiniComic, InstantiationTransform.position, Quaternion.identity);
-            ComicShown = true;
-            _showingComic = true;
-            _myGameManager.CurrentPOI = this.GetComponent<PointOfInterest>();
-            _myGameManager.FocusedOnPOI = true;
-        }
-    }
-
     void Update()
     {
         if (Physics2D.OverlapCircle(_myCamera.ScreenToWorldPoint(Input.mousePosition), 0.01f) && (Physics2D.OverlapCircle(_myCamera.ScreenToWorldPoint(Input.mousePosition), 0.01f).transform.parent.parent != null))

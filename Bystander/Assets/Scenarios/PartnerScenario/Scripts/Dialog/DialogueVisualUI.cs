cb0e222989a149b6e36a25656db4357073aeeb4c
mwarrington <mason.arrington@gmail.com>
2015-04-21 14:00:58 -0500

    Delay effect added

--- Bystander/Assets/Scenarios/PartnerScenario/Scripts/Dialog/DialogueVisualUI.cs
+++ Bystander/Assets/Scenarios/PartnerScenario/Scripts/Dialog/DialogueVisualUI.cs
@@ -3,34 +3,58 @@ using System.Collections.Generic;
 using PixelCrushers.DialogueSystem;
 using PixelCrushers.DialogueSystem.UnityGUI;
 
-public class DialogueVisualUI : UnityDialogueUI {
+public class DialogueVisualUI : UnityDialogueUI
+{
+    private bool _clickEnabled = true;
 
-	// This is a reference to the NPC subtitle fade effect. If the subtitle panel is already
-	// visible, we want to manually disable the fade effect so the panel stays visible.
-	public FadeEffect npcBorderFadeEffect;
+    // This is a reference to the NPC subtitle fade effect. If the subtitle panel is already
+    // visible, we want to manually disable the fade effect so the panel stays visible.
+    public FadeEffect npcBorderFadeEffect;
 
-	// We use this bool to remember whether the subtitle panel is already visible:
-	private bool isSubtitleVisible = false;
+    // We use this bool to remember whether the subtitle panel is already visible:
+    private bool isSubtitleVisible = false;
 
-	// Make sure npcBorderFadeEffect is assigned:
-	public override void Awake () {
-		base.Awake();
-		if (npcBorderFadeEffect == null) Debug.LogError(string.Format("{0}: NPC Border Fade Effect is unassigned!", DialogueDebug.Prefix));
-	}
+    // Make sure npcBorderFadeEffect is assigned:
+    public override void Awake()
+    {
+        base.Awake();
+        if (npcBorderFadeEffect == null) Debug.LogError(string.Format("{0}: NPC Border Fade Effect is unassigned!", DialogueDebug.Prefix));
+    }
 
-	// When the UI has just been opened, the subtitle panel is not visible:
-	public override void Open()	{
-		base.Open();
-		isSubtitleVisible = false;
-	}
+    // When the UI has just been opened, the subtitle panel is not visible:
+    public override void Open()
+    {
+        base.Open();
+        isSubtitleVisible = false;
+    }
 
-	// If the subtitle panel is already visible, disable the fade effect:
-	public override void ShowSubtitle(Subtitle subtitle) {
-		if (npcBorderFadeEffect != null) npcBorderFadeEffect.enabled = !isSubtitleVisible;
-		isSubtitleVisible = true;
-		base.ShowSubtitle(subtitle);
-	}
+    // If the subtitle panel is already visible, disable the fade effect:
+    public override void ShowSubtitle(Subtitle subtitle)
+    {
+        if (npcBorderFadeEffect != null) npcBorderFadeEffect.enabled = !isSubtitleVisible;
+        isSubtitleVisible = true;
+        base.ShowSubtitle(subtitle);
+    }
 
+    public override void OnContinue()
+    {
+        if (_clickEnabled)
+        {
+            _clickEnabled = false;
+            Invoke("EnableClick", 1.25f);
+            base.OnContinue();
+        }
+    }
+
+    public override void OnClick(object data)
+    {
+        if(_clickEnabled)
+        {
+            _clickEnabled = false;
+            Invoke("EnableClick", 1.25f);
+            base.OnClick(data);
+        }
+    }
 
     public override void ShowResponses(Subtitle subtitle, Response[] responses, float timeout)
     {
@@ -66,5 +90,10 @@ public class DialogueVisualUI : UnityDialogueUI {
         }
 
         base.ShowResponses(subtitle, responses, timeout);
+    }
+
+    public void EnableClick()
+    {
+        _clickEnabled = true;
     }
 }

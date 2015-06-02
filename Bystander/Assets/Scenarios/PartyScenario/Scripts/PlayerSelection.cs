f957e67c3c8982e1dd0a3b938e21ddddf3dad9cb
mwarrington <mason.arrington@gmail.com>
2015-06-02 16:15:09 -0500

    Player select almost done

--- Bystander/Assets/Scenarios/PartyScenario/Scripts/PlayerSelection.cs
+++ Bystander/Assets/Scenarios/PartyScenario/Scripts/PlayerSelection.cs
@@ -3,7 +3,46 @@ using System.Collections;
 
 public class PlayerSelection : MonoBehaviour
 {
-    public GameObject SelectObject;
+    protected bool mouseOverCoral
+    {
+        get
+        {
+            return _mouseOverCoral;
+        }
+
+        set
+        {
+            if (value != _mouseOverCoral)
+            {
+                if (value)
+                    _mouseOverDavid = false;
+
+                _mouseOverCoral = value;
+            }
+        }
+    }
+    private bool _mouseOverCoral;
+    protected bool mouseOverDavid
+    {
+        get
+        {
+            return _mouseOverDavid;
+        }
+
+        set
+        {
+            if (value != _mouseOverDavid)
+            {
+                if (value)
+                    _mouseOverCoral = false;
+
+                _mouseOverDavid = value;
+            }
+        }
+    }
+    private bool _mouseOverDavid;
+
+    public GameObject[] SelectObjects;
 
     private PartyGameManager _myGameManager;
     private SpriteRenderer _idleSprite;
@@ -19,23 +58,84 @@ public class PlayerSelection : MonoBehaviour
     {
         Collider2D overlapCircle = Physics2D.OverlapCircle(_myCamera.ScreenToWorldPoint(Input.mousePosition), 0.01f);
 
-        if (overlapCircle && overlapCircle.transform.parent != null)
+        if (overlapCircle && overlapCircle.transform != null)
         {
-            if (overlapCircle.transform.parent.transform == this.transform)
+            if (overlapCircle.gameObject == SelectObjects[0])
             {
-                SelectObject.SetActive(true);
+                RendererHandler(SelectObjects[0]);
+                SelectObjects[0].GetComponent<Animator>().SetBool("MousedOver", true);
 
                 if (Input.GetKeyUp(KeyCode.Mouse0))
-                    SelectionMade();
+                    SelectionMade(true);
+            }
+            else if (overlapCircle.gameObject == SelectObjects[1])
+            {
+                RendererHandler(SelectObjects[1]);
+                SelectObjects[1].GetComponent<Animator>().SetBool("MousedOver", true);
+
+                if (Input.GetKeyUp(KeyCode.Mouse0))
+                    SelectionMade(false);
             }
         }
     }
 
-    private void SelectionMade()
+    private void SelectionMade(bool isCoral)
     {
         if (this.name == "Coral")
             _myGameManager.ChoseCoral = true;
 
         Debug.Log("I suggest feather touch. You have selected " + this.name);
+    }
+
+    private void RendererHandler(GameObject selectObject)
+    {
+        if (SelectObjects[0] == selectObject)
+        {
+            SelectObjects[0].GetComponent<SpriteRenderer>().sortingOrder = 2;
+            SpriteRenderer[] otherRenderers = SelectObjects[0].GetComponentsInChildren<SpriteRenderer>();
+
+            for (int i = 0; i < otherRenderers.Length; i++)
+            {
+                if (otherRenderers[i].name == "FighterSelectFrameSprite")
+                    otherRenderers[i].sortingOrder = 4;
+                else
+                    otherRenderers[i].sortingOrder = 3;
+            }
+
+            SelectObjects[1].GetComponent<SpriteRenderer>().sortingOrder = 1;
+            otherRenderers = SelectObjects[1].GetComponentsInChildren<SpriteRenderer>();
+
+            for (int i = 0; i < otherRenderers.Length; i++)
+            {
+                if (otherRenderers[i].name == "FighterSelectFrameSprite")
+                    otherRenderers[i].sortingOrder = 3;
+                else
+                    otherRenderers[i].sortingOrder = 2;
+            }
+        }
+        else
+        {
+            SelectObjects[1].GetComponent<SpriteRenderer>().sortingOrder = 2;
+            SpriteRenderer[] otherRenderers = SelectObjects[1].GetComponentsInChildren<SpriteRenderer>();
+
+            for (int i = 0; i < otherRenderers.Length; i++)
+            {
+                if (otherRenderers[i].name == "FighterSelectFrameSprite")
+                    otherRenderers[i].sortingOrder = 4;
+                else
+                    otherRenderers[i].sortingOrder = 3;
+            }
+
+            SelectObjects[0].GetComponent<SpriteRenderer>().sortingOrder = 1;
+            otherRenderers = SelectObjects[0].GetComponentsInChildren<SpriteRenderer>();
+
+            for (int i = 0; i < otherRenderers.Length; i++)
+            {
+                if (otherRenderers[i].name == "FighterSelectFrameSprite")
+                    otherRenderers[i].sortingOrder = 3;
+                else
+                    otherRenderers[i].sortingOrder = 2;
+            }
+        }
     }
 }

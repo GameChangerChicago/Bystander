f40a8c46bc538da3fe05db2bc293d0dccf47fad2
kaira2006 <kayura60@gmail.com>
2015-06-28 18:08:01 -0500

    Created Hubworlds for each of the minigames
    Should probably create a singleton pattern for the hub world however
    Unity makes design patterns difficult…so…yeah

---
+++ Bystander/Assets/Scenarios/Main Menu/Scripts/HubWorldManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public enum HB_GameState
+{
+	PrePartner,
+	PreParty,
+	PreHarassment,
+	PreMale,
+}
+;
+
+public class HubWorldManager : MonoBehaviour {
+
+
+
+	private string nextLevel;
+	public static HB_GameState gameState;
+	private int i;
+	private string[] level_Names;
+	public List<GameObject> LevelDescriptions;
+
+
+	void Awake(){
+
+		DontDestroyOnLoad (this);
+		}
+
+	// Use this for initialization
+	void Start () {
+	
+		i = 0;
+		level_Names = Enum.GetNames (typeof(HB_GameState)); 
+		nextLevel = level_Names [i];
+		LevelDescriptions [i].SetActive (true);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+	public void prepNextLevel ()
+	{
+		LevelDescriptions [i].SetActive (false);
+		i++;
+		nextLevel = level_Names [i];
+		LevelDescriptions [i].SetActive (true);
+
+	}
+
+
+	public string NextLevel ()
+	{
+		return nextLevel;
+	}
+}
---
+++ Bystander/Assets/Scenarios/Main Menu/Scripts/PlayButton.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayButton : MonoBehaviour {
+
+	public GameObject regularSprite, selectedSprite;
+	private HubWorldManager hb_Manager;
+	private HB_GameState gameState;
+	public string level;
+
+	// Use this for initialization
+	void Start () {
+
+		hb_Manager = FindObjectOfType<HubWorldManager> ();
+		//gameState = hb_Manager.isGameState();
+		regularSprite.GetComponent<SpriteRenderer>().enabled = true;
+		selectedSprite.GetComponent<SpriteRenderer>().enabled = false;
+
+
+
+	}
+
+
+	void OnMouseOver(){
+
+		selectedSprite.GetComponent<SpriteRenderer>().enabled = true;
+		Debug.Log ("WTF 2");
+
+		}
+
+	void OnMouseExit(){
+
+		selectedSprite.GetComponent<SpriteRenderer>().enabled = false;
+		Debug.Log ("WTF 3");
+
+	}
+
+	void OnMouseDown(){
+
+		Application.LoadLevel (level);
+		//Application.LoadLevel (hb_Manager.NextLevel());
+		//hb_Manager.prepNextLevel ();
+	}
+
+}

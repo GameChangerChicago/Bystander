2720cb5638a0ec9e17ca33afd1935c7ba60dd057
mwarrington <mason.arrington@gmail.com>
2015-08-20 10:53:04 -0500

    Added cursor for Hub World scenes

--- Bystander/Assets/Scenarios/Main Menu/Scripts/PlayButton.cs
+++ Bystander/Assets/Scenarios/Main Menu/Scripts/PlayButton.cs
@@ -4,13 +4,14 @@ using System.Collections;
 public class PlayButton : MonoBehaviour {
 
 	public GameObject regularSprite, selectedSprite;
+    private CursorHandler _cursorHandler;
 	private HubWorldManager hb_Manager;
 	private HB_GameState gameState;
 	public string level;
 
 	// Use this for initialization
 	void Start () {
-
+        _cursorHandler = FindObjectOfType<CursorHandler>();
 		hb_Manager = FindObjectOfType<HubWorldManager> ();
 		//gameState = hb_Manager.isGameState();
 		regularSprite.GetComponent<SpriteRenderer>().enabled = true;
@@ -22,14 +23,14 @@ public class PlayButton : MonoBehaviour {
 
 
 	void OnMouseOver(){
-
+        _cursorHandler.ChangeCursor(0);
 		selectedSprite.GetComponent<SpriteRenderer>().enabled = true;
 		Debug.Log ("WTF 2");
 
 		}
 
 	void OnMouseExit(){
-
+        _cursorHandler.ChangeCursor(1);
 		selectedSprite.GetComponent<SpriteRenderer>().enabled = false;
 		Debug.Log ("WTF 3");
 

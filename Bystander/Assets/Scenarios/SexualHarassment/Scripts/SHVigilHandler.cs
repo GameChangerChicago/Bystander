06a13f1f090ff1def652eae057a33ee9625711a5
Phehrenberg <phehrenberg@gmail.com>
2015-10-12 15:44:15 -0500

    Harassment Cutscene Polish
    Text/grammar edits throughout, animations and transitions in pre and
    post.

--- Bystander/Assets/Scenarios/SexualHarassment/Scripts/SHVigilHandler.cs
+++ Bystander/Assets/Scenarios/SexualHarassment/Scripts/SHVigilHandler.cs
@@ -77,8 +77,8 @@ public class SHVigilHandler : MonoBehaviour
         _myAudioSource = GetComponentInChildren<AudioSource>();
         _wrongAnswerClip = Resources.Load("Sounds/04-Reminder") as AudioClip;
         _interventionClip = Resources.Load("Sounds/05-Interventions") as AudioClip;
-        _correctString = "That's right! But let's try to find an instance of sexual harrasment. ";
-        _wrongString = "It seems like you're having some trouble. Remember, we're trying to find an instance of sexual harrasment. ";
+        _correctString = "That's right! But let's try to find an instance of sexual harassment. ";
+        _wrongString = "It seems like you're having some trouble. Remember, we're trying to find an instance of sexual harassment. ";
     }
 
     //Clicking anywhere will call ShowSpringSegment
